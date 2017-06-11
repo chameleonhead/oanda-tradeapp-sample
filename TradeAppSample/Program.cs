65a3c52be06a9adb003c8b2fee585b53a7be48f6
Yuto Nagano <chamelion@hotmail.co.jp>
2017-06-11 23:19:13 +0900

    アプリケーションの体裁を作成

--- TradeAppSample/Program.cs
+++ TradeAppSample/Program.cs
@@ -10,6 +10,16 @@ namespace TradeAppSample
     {
         static void Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine(@"引数は必ず2つ指定してください。
+引数1: アクセストークン
+引数2: アカウント");
+            }
+            string key = args[0];
+            int accountId = int.Parse(args[1]);
+            var app = new TradeApp(key, accountId);
+            app.Run();
         }
     }
 }
---
+++ TradeAppSample/TradeApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rabun.Oanda.Rest.Endpoints;
+using Rabun.Oanda.Rest.Factories;
+using Rabun.Oanda.Rest.Base;
+
+namespace TradeAppSample
+{
+    public class TradeApp
+    {
+        public TradeApp(string key, int accountId)
+        {
+            var factory = new DefaultFactory(key, AccountType.practice, accountId);
+            OrderEndPoints = factory.GetEndpoint<OrderEndpoints>();
+            PositionEndpoints = factory.GetEndpoint<PositionEndpoints>();
+            RateEndpoints = factory.GetEndpoint<RateEndpoints>();
+            TradeEndpoints = factory.GetEndpoint<TradeEndpoints>();
+            TransactionEndpoints = factory.GetEndpoint<TransactionEndpoints>();
+        }
+
+        public OrderEndpoints OrderEndPoints { get; internal set; }
+        public PositionEndpoints PositionEndpoints { get; internal set; }
+        public RateEndpoints RateEndpoints { get; internal set; }
+        public TradeEndpoints TradeEndpoints { get; internal set; }
+        public TransactionEndpoints TransactionEndpoints { get; internal set; }
+
+        /// <summary>
+        /// プログラム実行ループ
+        /// </summary>
+        public void Run()
+        {
+
+        }
+    }
+}
