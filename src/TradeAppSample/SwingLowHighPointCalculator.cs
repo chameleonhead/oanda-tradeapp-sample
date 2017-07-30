90593d145d23bda4bfd7391467a5de84e8738f28
Yuto Nagano <chamelion@hotmail.co.jp>
2017-07-30 23:26:33 +0900

    SwingLow/SwingHighの価格計算を実装

---
+++ src/TradeAppSample/SwingLowHighPointCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace TradeAppSample
+{
+    public class SwingLowHighPointResult
+    {
+        internal SwingLowHighPointResult(ForexPrice high, ForexPrice low)
+        {
+            Low = low;
+            High = high;
+        }
+
+        public ForexPrice Low { get; }
+        public ForexPrice High { get; }
+    }
+
+    public class SwingLowHighPointCalculator
+    {
+        public SwingLowHighPointResult Calculate(ForexCandle[] candles)
+        {
+            var high = candles.Max(x => x.High);
+            var low = candles.Min(x => x.Low);
+            return new SwingLowHighPointResult(high, low);
+        }
+    }
+}
---
+++ tests/TradeAppSample.Test/SwingLowHighPointCalculatorTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+
+namespace TradeAppSample.Test
+{
+    [TestClass]
+    public class SwingLowHighPointCalculatorTest
+    {
+        [TestMethod]
+        public void ShouldRecognizeSwingHighPointAndSwingLowPointFromPassedLine()
+        {
+            var candles = new ForexCandle[] {
+                ForexCandleUtils.Create(100, 110, 90, 101),
+                ForexCandleUtils.Create(101, 102, 90, 90),
+                ForexCandleUtils.Create(90, 111, 89, 110),
+                ForexCandleUtils.Create(110, 120, 100, 115),
+                ForexCandleUtils.Create(115, 117, 113, 110),
+            };
+            var calculator = new SwingLowHighPointCalculator();
+            var result = calculator.Calculate(candles);
+            Assert.AreEqual(120, result.High.Bid);
+            Assert.AreEqual(89, result.Low.Bid);
+        }
+    }
+}
